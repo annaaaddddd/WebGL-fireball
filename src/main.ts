880caedaa1ad0443553e9ed26b0b1614bc274330
annaaaddddd <anna.dai4103@gmail.com>
2026-09-21 15:38:48 -0400

    Added uTime for shaders

--- src/main.ts
+++ src/main.ts
@@ -68,7 +68,9 @@ function main() {
   ]);
 
   // This function will be called every frame
-  function tick() {
+  function tick(timeMs: number) {
+    const time = timeMs / 1000.0;
+    lambert.setTime(time);
     camera.update();
     stats.begin();
     gl.viewport(0, 0, window.innerWidth, window.innerHeight);
@@ -100,7 +102,7 @@ function main() {
   camera.updateProjectionMatrix();
 
   // Start the render loop
-  tick();
+  requestAnimationFrame(tick);
 }
 
 main();
--- src/shaders/lambert-vert.glsl
+++ src/shaders/lambert-vert.glsl
@@ -18,6 +18,7 @@ uniform mat4 u_ModelInvTr;  // The inverse transpose of the model matrix.
 uniform mat4 u_ViewProj;    // The matrix that defines the camera's transformation.
                             // We've written a static matrix for you to use for HW2,
                             // but in HW3 you'll have to generate one yourself
+uniform float u_Time;
 
 in vec4 vs_Pos;             // The array of vertex positions passed to the shader
 
